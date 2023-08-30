54eec6a10a6e657bc177d8873fa42832be28d1b7
CardinalCandle <youssef-kj@outlook.fr>
2023-08-30 18:47:39 +0100

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import {
+  ThemeProvider as MuiThemeProvider,
+  createTheme,
+} from "@material-ui/core/styles";
 import Navbar from "./components/Navbar";
-
 // Pages
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#3374dc",
+      main: "#0051d4",
+      dark: "#003994",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ffc933",
+      main: "#ffbb00",
+      dark: "#b28300",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 export class App extends Component {
   render() {
     return (
-      <div className="App">
-        App
-        <BrowserRouter>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/signup" element={<Signup />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          App
+          <BrowserRouter>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/Post.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// Material UI
+import {
+  CardMedia,
+  Typography,
+  Card,
+  CardContent,
+  withStyles,
+} from "@material-ui/core";
+
+const styles = {
+  card: {
+    display: "flex",
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover",
+  },
+};
+export class Post extends Component {
+  render() {
+    const {
+      classes,
+      post: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        postId,
+        likeCount,
+        commentCount,
+      },
+    } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile Image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(Post);
