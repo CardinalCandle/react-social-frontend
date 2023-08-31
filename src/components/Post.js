fb13cd9c103fa37c809bb127867e10e382906f72
CardinalCandle <youssef-kj@outlook.fr>
2023-08-31 04:32:19 +0100

    Added login page some issues persist : the handleSubmit function doesn't redirect in accordance with React's ecosystem but using plain javascript, states are not preserved,
    this is mainly because react router dom v6 has decided to fuck me over
    and removed the history.push capability, so now i'm stuck using a bunch
    of Navigate stuff of which I know nothing and that don't even work in
    this case, so I used a plain js workaround, until I get some much needed
     experience in the abomination that is router dom v6

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 // Material UI
 import {
   CardMedia,
@@ -24,6 +26,7 @@ const styles = {
 };
 export class Post extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       post: {
@@ -39,6 +42,7 @@ export class Post extends Component {
     return (
       <Card className={classes.card}>
         <CardMedia
+          component="div"
           image={userImage}
           title="Profile Image"
           className={classes.image}
@@ -53,7 +57,7 @@ export class Post extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,154 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  Grid,
+  withStyles,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+} from "@material-ui/core";
+import { ReactComponent as AppIcon } from "../images/icon.svg";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    margin: "20px auto",
+  },
+  pageTitle: {
+    margin: "10px auto",
+  },
+  TextField: {
+    margin: "10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position: "relative",
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+  },
+  progress: {
+    position: "absolute",
+  },
+};
 
-export class Login extends Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+  handleSubmit = (event) => {
+    this.setState({ loading: true });
+    event.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/login", userData)
+      .then((res) => {
+        this.setState({ loading: false, loggedIn: true });
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        console.log("submit4", err);
+        this.setState({
+          errors: err.response,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
-      <div>
-        <h1>Login page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <AppIcon
+            height={75}
+            width={75}
+            fill="indigo"
+            className={classes.image}
+          />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.TextField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              className={classes.TextField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+              className={classes.button}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={20} className={classes.progress} />
+              )}
+            </Button>
+
+            <br />
+            <small>
+              don't have an account ? sign up <Link to="/signup">here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
